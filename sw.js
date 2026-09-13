791993e7dddde38d892f2451fdce229390527691
scccr7311-hub <scccr7311@gmail.com>
2026-09-13 19:24:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,22 @@
-const CACHE = 'voice-memo-v1'
-const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png']
+const CACHE = 'voice-memo-v2'
+const FILES = ['./manifest.webmanifest', './icon-192.png', './icon-512.png']
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)).then(() => self.skipWaiting()))
 })
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(self.clients.claim())
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  )
 })
 
 self.addEventListener('fetch', (e) => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(fetch(e.request).catch(() => caches.match('./index.html')))
+    return
+  }
   e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)))
 })
