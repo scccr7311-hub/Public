0f7a1bec659ba08cd22ae8c66decb397d791d20b
scccr7311-hub <scccr7311@gmail.com>
2026-09-13 15:51:11 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'voice-memo-v1'
+const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png']
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)).then(() => self.skipWaiting()))
+})
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)))
+})
